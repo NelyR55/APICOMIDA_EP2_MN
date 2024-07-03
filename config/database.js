c4df103678cccc50c3e6483ec085a05b2f7c0945
Nely Rodriguez <nelyr844@gmail.com>
2024-07-02 20:56:53 -0600

    CONEXION RAILWAY

--- config/database.js
+++ config/database.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
+// Cargar las variables de entorno desde un archivo .env
+
+
+// Función de conexión a la base de datos
 const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb://localhost:27017/apicomida', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Conectado con MongoDB...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
+    try {
+        mongoose.set('strictQuery', true);
+
+        // Conectar a la base de datos usando la URL de conexión de Railway
+        await mongoose.connect('mongodb://mongo:YVZvPGbbrsjHZnQRfyQBWfEgJhAGbfyV@monorail.proxy.rlwy.net:52173');
+        console.log("Conexión correcta");
+    } catch (error) {
+        console.error("Error en la conexión", error);
+    }
 };
 
 module.exports = connectDB;
